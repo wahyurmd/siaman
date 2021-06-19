583836b7e94825281eaa319d2cca14ec0c27b7c0
jabbar-hafizh <jabbarworld1@gmail.com>
2021-06-19 12:35:46 +0700

    fix multipe insert into nilai

--- app/Http/Controllers/NilaiController.php
+++ app/Http/Controllers/NilaiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Nilai;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -12,22 +13,53 @@ class NilaiController extends Controller
     {
         date_default_timezone_set('Asia/Jakarta');
 
-        $id_kelas = $request->id_kelas;
-        $nisn = $request->nisn;
-        $id_mapel = $request->id_mapel;
-        $id_semester = $request->id_semester;
-        $uts = $request->uts;
+        $data = $request->all();
+        $jml_data = count($data['nisn']);
+        // dd($data['id_kelas']);
 
-        foreach ($id_kelas as $key) {
-            DB::table('nilai')->insert([
-                'id_kelas'      => $id_kelas[$key],
-                'nisn'          => $nisn[$key],
-                'id_mapel'      => $id_mapel[$key],
-                'id_semester'   => $id_semester[$key],
-                'uts'           => $uts[$key],
+        for ($i=0; $i < $jml_data; $i++) {
+            $data2 = array(
+                'id_kelas'      => $data['id_kelas'][$i],
+                'nisn'          => $data['nisn'][$i],
+                'id_mapel'      => $data['id_mapel'][$i],
+                'id_semester'   => $data['id_semester'][$i],
+                'uts'           => $data['uts'][$i],
                 'created_at'    => Carbon::now()->toDateTimeString()
-            ]);
+            );
+            Nilai::create($data2);
         }
+
+        // if ($jml_data > 0) {
+        //     foreach ($data['id_kelas'] as $i => $value) {
+        //         // dd($data['nisn'][$i]);
+        //         $data2 = array(
+        //             'id_kelas'      => $data['id_kelas'][$i],
+        //             'nisn'          => $data['nisn'][$i],
+        //             'id_mapel'      => $data['id_mapel'][$i],
+        //             'id_semester'   => $data['id_semester'][$i],
+        //             'uts'           => $data['uts'][$i],
+        //             'created_at'    => Carbon::now()->toDateTimeString()
+        //         );
+        //         Nilai::create($data2);
+        //     }
+        // }
+
+        // $id_kelas = $request->id_kelas;
+        // $nisn = $request->nisn;
+        // $id_mapel = $request->id_mapel;
+        // $id_semester = $request->id_semester;
+        // $uts = $request->uts;
+
+        // foreach ($id_kelas as $i) {
+        //     DB::table('nilai')->insert([
+        //         'id_kelas'      => $id_kelas[$i],
+        //         'nisn'          => $nisn[$i],
+        //         'id_mapel'      => $id_mapel[$i],
+        //         'id_semester'   => $id_semester[$i],
+        //         'uts'           => $uts[$i],
+        //         'created_at'    => Carbon::now()->toDateTimeString()
+        //     ]);
+        // }
 
         return redirect('guru/input_uts');
     }
